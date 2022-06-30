d0ee1b73f2480580a9652796b79c90b0a7ffc55b
astrezhnev_metadevs <astrezhnev@meta-devs.io>
2022-06-30 18:31:05 +0500

    oop add

---
+++ src/com/metadevs/oop/Worker.java
@@ -0,0 +1,16 @@
+package com.metadevs.oop;
+
+public interface Worker {
+    void coolerTalk();
+    void smoking();
+    void getVacation();
+    void getLunchBreak();
+    void haveChild();
+    String getName();
+    boolean checkDifference();
+    void aboutPerson();
+    String getCompanyName();
+    void setCompanyName(String companyName);
+    void setSalary(int salary);
+
+}
